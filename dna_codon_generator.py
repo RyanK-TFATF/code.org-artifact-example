6c3f4519000662de8dda8074cf42be026f824243
Ryan Kelley <92961892+RyanK-TFATF@users.noreply.github.com>
2022-04-30 01:40:08 -0400

    Update dna_codon_generator.py
    Created the basic while loop to generate the random number and corresponding base.

--- dna_codon_generator.py
+++ dna_codon_generator.py
@@ -14,3 +14,16 @@ dnaSequence = ""
 
 basesGenerated = 0
 # Variable to count how many bases have been generated. 
+
+while basesGenerated <= 100: # The teacher will specify how many bases to generate.  
+    randomBase = int(random.randint(1,4)) # This simulates rolling the d4 to get a random number. 
+
+    if randomBase == 1:
+        nextBase = dnaBases[0] # This references the first element in the dnaBases list. 
+    elif randomBase == 2:
+        nextBase = dnaBases[1] # This references the second element in the dnaBases list. 
+    elif randomBase == 3:
+        nextBase = dnaBases[2] # This references the third element in the dnaBases list. 
+    else:
+        nextBase = dnaBases[3] # This references the fourth element in the dnaBases list. 
+    
