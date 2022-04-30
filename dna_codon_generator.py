29cb4dfea6c84b670c4222893adfe9b3d26a77f5
Ryan Kelley <92961892+RyanK-TFATF@users.noreply.github.com>
2022-04-30 01:32:09 -0400

    Create dna_codon_generator.py
    Import the random module.
    Create the LISTS for the DNA bases and the DNA codons.

---
+++ dna_codon_generator.py
@@ -0,0 +1,11 @@
+# Ryan Kelley, 2022 code.org 6-12 Curriculum Development Manager Example Artifact 
+
+import random 
+
+dnaBases = ["A", "G", "C", "T"]
+# Adenine, Guanine, Cytosine, Thymine
+
+dnaCodons = ["ATG", "TAG"]
+# ATG is the START codon, TAG is one of the stop codons.  
+# Codon information referenced here:  https://www.genome.gov/genetics-glossary/Codon and https://en.wikipedia.org/wiki/DNA_and_RNA_codon_tables#Inverse_DNA_codon_table
+
