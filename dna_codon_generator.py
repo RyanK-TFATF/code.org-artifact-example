e3f2da49e1b48026b92cd69c2f112b07eeba58ca
Ryan Kelley <92961892+RyanK-TFATF@users.noreply.github.com>
2022-04-30 02:02:49 -0400

    Update dna_codon_generator.py
    Testing for loop to check for each codon from the dnaCodons list.
    
    Adjusted strings to indicate that only the first instance of each codon is returned by .find()

--- dna_codon_generator.py
+++ dna_codon_generator.py
@@ -5,7 +5,7 @@ import random
 dnaBases = ["A", "G", "C", "T"]
 # Adenine, Guanine, Cytosine, Thymine
 
-dnaCodons = ["ATG", "TAG"]
+dnaCodons = ["ATG", "TAG", "GAT", "TTT"]
 # ATG is the START codon, TAG is one of the stop codons.  
 # Codon information referenced here:  https://www.genome.gov/genetics-glossary/Codon and https://en.wikipedia.org/wiki/DNA_and_RNA_codon_tables#Inverse_DNA_codon_table
 
@@ -15,7 +15,7 @@ dnaSequence = ""
 basesGenerated = 0
 # Variable to count how many bases have been generated. 
 
-while basesGenerated <= 100000: # The teacher will specify how many bases to generate.  
+while basesGenerated <= 100: # The teacher will specify how many bases to generate.  
     randomBase = int(random.randint(1,4)) # This simulates rolling the d4 to get a random number. 
 
     if randomBase == 1:
@@ -32,6 +32,7 @@ while basesGenerated <= 100000: # The teacher will specify how many bases to gen
 
 print(dnaSequence) # Print the sequence to verify proper generation. 
 
+"""
 if dnaSequence.find(dnaCodons[0]) == -1: # .find() returns -1 if not found. 
     print(f"The {dnaCodons[0]} codon was NOT found in the generated sequence.\n")
 else: 
@@ -41,3 +42,16 @@ if dnaSequence.find(dnaCodons[1]) == -1:
     print(f"The {dnaCodons[1]} codon was NOT found in the generated sequence.\n")
 else: 
     print(f"The {dnaCodons[1]} codon was found!  The first instance starts at index {dnaSequence.find(dnaCodons[1])}.\n")
+"""
+
+for i in range(0, len(dnaCodons)):
+    if dnaSequence.find(dnaCodons[i]) == -1: # .find() returns -1 if not found. 
+        print(f"The {dnaCodons[i]} codon was NOT found in the generated sequence.\n")
+    else: 
+        print(f"The {dnaCodons[i]} codon was found!  The first instance starts at index {dnaSequence.find(dnaCodons[i])}.\n") # Return the index of the first instance of the codon.  
+    """
+    if dnaSequence.find(dnaCodons[i]) == -1:
+        print(f"The {dnaCodons[i]} codon was NOT found in the generated sequence.\n")
+    else: 
+        print(f"The {dnaCodons[i]} codon was found!  The first instance starts at index {dnaSequence.find(dnaCodons[i])}.\n")
+    """ 
