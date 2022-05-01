1c5dde2d66c17315e609e239d4480acf315e8515
Ryan Kelley <92961892+RyanK-TFATF@users.noreply.github.com>
2022-04-30 23:29:00 -0400

    v0.5b
    Added variable to control while loop, dependent on number of bases requested.
    
    Cleaned up comments.
    
    Removed the if-elif-else block as it is no longer needed.

--- dna_codon_generator-lists.py
+++ dna_codon_generator-lists.py
@@ -1,5 +1,6 @@
-# Ryan Kelley, 2022 code.org 6-12 Curriculum Development Manager Example Artifact, v0.4.1b
+# Ryan Kelley, 2022 code.org 6-12 Curriculum Development Manager Example Artifact, v0.5b
 
+# Import the random module to allow for random integer generation to simulate rolling the d4.
 import random 
 
 dnaBases = ["A", "G", "C", "T"]
@@ -9,25 +10,19 @@ dnaCodons = ["ATG", "TAG", "GAT", "TTT", "AGC"]
 # ATG is the START codon, TAG is one of the stop codons.  
 # Codon information referenced here:  https://www.genome.gov/genetics-glossary/Codon and https://en.wikipedia.org/wiki/DNA_and_RNA_codon_tables#Inverse_DNA_codon_table
 
-dnaSequence = "" 
 # Create an empty string variable to store the DNA sequence. 
+dnaSequence = "" 
 
-basesGenerated = 0
+
 # Variable to count how many bases have been generated. 
+basesGenerated = 0
 
-while basesGenerated <= 10: # The teacher will specify how many bases to generate.  
+# Variable to control while loop, allows user to specify the number of bases to generate. 
+requestedBases = int(input("How many DNA bases do you require in the sequence?  Type an integer value and press ENTER.\n"))
+
+while basesGenerated < requestedBases: # The teacher will specify how many bases to generate.  
     dnaSequence += dnaBases[int(random.randint(0,3))] # This simulates rolling the d4 to get a random number, start with 0 instead of 1 to ensure first element can be selected. 
     basesGenerated += 1 # Increment the number of bases generated. 
-    """"
-    if randomBase == 1:
-        nextBase = dnaBases[0] # This references the first element in the dnaBases list. 
-    elif randomBase == 2:
-        nextBase = dnaBases[1] # This references the second element in the dnaBases list. 
-    elif randomBase == 3:
-        nextBase = dnaBases[2] # This references the third element in the dnaBases list. 
-    else:
-        nextBase = dnaBases[3] # This references the fourth element in the dnaBases list. 
-    """
     
 print(f"\nGenerated DNA Sequence: {dnaSequence}\n\n") # Print the sequence to verify proper generation. 
 
